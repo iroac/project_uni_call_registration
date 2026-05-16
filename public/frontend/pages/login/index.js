e30f1994534d371eb38d52b075081b8fd984639d
roacode <vns5218@icloud.com>
2026-05-16 12:53:24 -0300

    refactor: streamline form submission handling in login component

--- public/frontend/pages/login/index.js
+++ public/frontend/pages/login/index.js
@@ -5,23 +5,20 @@ const form = document.getElementById("formLogin");
 
 form.addEventListener("submit", async (event) => {
 	event.preventDefault();
-	const formUtils = new FormUtils(form, "login");
+	const { isSubmitting, notify, data } = new FormUtils(form, "login");
 
 	try {
-		formUtils.isSubmitting(true);
-		const response = await postLogin(formUtils.data);
+		isSubmitting(true);
+		const response = await postLogin(data);
 		const { message, error } = await response.json();
 
-		formUtils.notify(
+		notify(
 			error || message || "Login realizado com sucesso!",
 			error ? "danger" : "success",
 		);
 	} catch (error) {
-		formUtils.notify(
-			"Erro ao fazer login. Por favor, tente novamente.",
-			"danger",
-		);
+		notify("Erro ao fazer login. Por favor, tente novamente.", "danger");
 	} finally {
-		formUtils.isSubmitting(false);
+		isSubmitting(false);
 	}
 });
