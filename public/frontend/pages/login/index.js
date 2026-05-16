9620c3ff3503c5e96873e99bdb22df2754acbb2a
roacode <vns5218@icloud.com>
2026-05-16 12:07:10 -0300

    feat: refactor login form handling and improve button state management

--- public/frontend/pages/login/index.js
+++ public/frontend/pages/login/index.js
@@ -1,41 +1,21 @@
 import { postLogin } from "../../global/api.js";
-import { getFormData, isSubmitting } from "../../utils/form.js";
+import { FormUtils } from "../../utils/form.js";
 
 const form = document.getElementById("formLogin");
 
-const toggleButtonLoadingState = (isLoading) => {
-	const loginButton = form.querySelector("button[type='submit']");
-	const spinner = document.getElementById("loginButtonSpinner");
-	if (isLoading) {
-		loginButton.disabled = true;
-		spinner.classList.remove("d-none");
-	} else {
-		loginButton.disabled = false;
-		spinner.classList.add("d-none");
-	}
-};
-
-const disableLoginFields = (isDisabled) => {
-	const emailField = document.getElementById("loginEmail");
-	const passwordField = document.getElementById("loginPassword");
-	emailField.disabled = isDisabled;
-	passwordField.disabled = isDisabled;
-};
-
 form.addEventListener("submit", async (event) => {
 	event.preventDefault();
-	const data = getFormData(form);
+	const formUtils = new FormUtils(form, "login");
 
 	try {
-		isSubmitting(true);
-		const response = await postLogin(data);
-
-		const { message, error } = await response.json();
+		formUtils.isSubmitting(true);
+		const response = await postLogin(formUtils.data);
+		const { message, error } = response.json();
 
 		console.log("Resposta do servidor:", message || error);
 	} catch (error) {
 		console.error("Erro ao fazer login:", error);
 	} finally {
-		isSubmitting(false);
+		formUtils.isSubmitting(false);
 	}
 });
--- public/frontend/utils/form.js
+++ public/frontend/utils/form.js
@@ -1,15 +1,50 @@
-export const isSubmitting = (isSubmitting) => {
-	disableLoginFields(isSubmitting);
-	toggleButtonLoadingState(isSubmitting);
-};
+export class FormUtils {
+	data = {};
+	#fields = [];
 
-export const getFormData = (form) => {
-	const formData = new FormData(form);
-	const data = {};
+	#initFormData() {
+		const formData = new FormData(this.formElement);
+		const data = {};
 
-	for (const [key, value] of formData.entries()) {
-		data[key] = value;
+		for (const [key, value] of formData.entries()) {
+			data[key] = value;
+		}
+
+		this.data = data;
+		this.#fields = Object.keys(data);
 	}
 
-	return data;
-};
+	constructor(formElement, action) {
+		this.formElement = formElement;
+		this.action = action; // é o id do form, deve ser unico por form na página;
+		this.#initFormData();
+	}
+
+	toggleButtonLoadingState = (isLoading) => {
+		const button = this.formElement.querySelector("button[type='submit']");
+		const spinner = document.getElementById(`${this.action}-button-spinner`);
+
+		if (isLoading) {
+			button.disabled = true;
+			spinner.classList.remove("d-none");
+		} else {
+			button.disabled = false;
+			spinner.classList.add("d-none");
+		}
+	};
+
+	disableLoginFields = (isDisabled) => {
+		for (const field of this.#fields) {
+			const inputField = document.getElementById(`${this.action}-${field}`);
+
+			if (inputField) {
+				inputField.disabled = isDisabled;
+			}
+		}
+	};
+
+	isSubmitting = (isSubmitting) => {
+		this.disableLoginFields(isSubmitting);
+		this.toggleButtonLoadingState(isSubmitting);
+	};
+}
