359d36771246206bc134131b343d1ae555cb168c
roacode <vns5218@icloud.com>
2026-05-17 12:23:42 -0300

    feat: update form fields for better accessibility and add autocomplete attributes

--- public/frontend/pages/cadastro/index.js
+++ public/frontend/pages/cadastro/index.js
@@ -1,5 +1,13 @@
+import { FormUtils } from "../../utils/form.js";
+
 const formCadastro = document.getElementById("formCadastro");
 
 formCadastro.addEventListener("submit", async (event) => {
 	event.preventDefault();
+	const { isSubmitting, notify, data } = new FormUtils(
+		formCadastro,
+		"cadastro",
+	);
+
+	console.log("Dados do formulário:", data); // Verifique os dados do formulário
 });
